9ccd9129194b6eb21db50f7f4ed1b65ebae59000
VieDev <piri.viejo@gmail.com>
2021-09-27 21:50:41 -0300

    Se agrego canvas no funca pero tiene trama css

--- javaScript.js
+++ javaScript.js
@@ -17,4 +17,62 @@
           form.classList.add('was-validated')
         }, false)
       })
-  })()
+  })();
+// Data
+const dataChartOptionsExample = {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          label: 'Traffic',
+          data: [30, 15, 62, 65, 61, 6],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  };
+  
+  // Options
+  const optionsChartOptionsExample = {
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              fontColor: '#4285F4',
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              fontColor: '#f44242',
+            },
+          },
+        ],
+      },
+    },
+  };
+  
+  new mdb.Chart(
+    document.getElementById('chart-options-example'),
+    dataChartOptionsExample,
+    optionsChartOptionsExample
+  );
